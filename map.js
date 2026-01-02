9f30d530c4862094b693c341de1a640597a4c71a
Kiss Zoltán <zolee1988@gmail.com>
2026-01-02 08:22:21 +0100

    Update map.js

--- map.js
+++ map.js
@@ -9,7 +9,11 @@ L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
 // Klub ikon (egyelőre a default Leaflet ikon)
 const clubIcon = L.icon({
     iconUrl: 'https://unpkg.com/leaflet@1.9.4/dist/images/marker-icon.png',
-    shadowUrl: 'https://unpkg.com/leaflet@1.9.4/dist/images/marker-shadow.png'
+    shadowUrl: 'https://unpkg.com/leaflet@1.9.4/dist/images/marker-shadow.png',
+    iconSize: [25, 41],
+    iconAnchor: [12, 41],
+    popupAnchor: [1, -34],
+    shadowSize: [41, 41]
 });
 
 // Wiki oldal lekérése
